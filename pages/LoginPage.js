dbdd4790f27bb9bcaf0158fc6859c5193884652b
lucasforlinn <lucasforlinn@gmail.com>
2025-03-04 10:26:53 -0300

    feat: implementing POM to the project

--- pages/LoginPage.js
+++ pages/LoginPage.js
@@ -1,1 +1,26 @@
- 
+export class LoginPage {
+    constructor(page) {
+        this.page = page
+    }
+
+    async acessaPagina() {
+        await this.page.goto('http://paybank-mf-auth:3000/');
+    }
+
+    async fillCPF(cpf) {
+        await this.page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(cpf);
+        await this.page.getByRole('button', { name: 'Continuar' }).click();
+    }
+
+    async fillPassword(password) {
+        for (const digito of password) {
+            await this.page.getByRole('button', { name: digito }).click();
+        }
+        await this.page.getByRole('button', { name: 'Continuar' }).click();
+    }
+
+    async fill2FCode(code) {
+        await this.page.getByRole('textbox', { name: '000000' }).fill(code);
+        await this.page.getByRole('button', { name: 'Verificar' }).click();
+    }
+}
--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,55 +1,46 @@
 import { test, expect } from '@playwright/test';
 import { get2FCode } from '../support/db.js';
+import { LoginPage } from '../pages/LoginPage.js';
+import { DashPage } from '../pages/DashPage.js';
 
 test('Try to login using a invalid 2F code', async ({ page }) => {
-  await page.goto('http://paybank-mf-auth:3000/');
-
+  const loginPage = new LoginPage(page);
   const user = {
     cpf: '00000014141',
-    senha: '147258'
+    password: '147258'
   }
 
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(user.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
+  await loginPage.acessaPagina();
 
+  await loginPage.fillCPF(user.cpf);
+  await loginPage.fillPassword(user.password);
+ 
+  await loginPage.fill2FCode('123456');
 
-  for (const digito of user.senha){
-    await page.getByRole('button', { name: digito }).click();
-  }
-
-  await page.getByRole('button', { name: 'Continuar' }).click();    
-
-  await page.getByRole('textbox', { name: '000000' }).fill('123456');
-  await page.getByRole('button', { name: 'Verificar' }).click();
-
-  
   await expect(page.locator('span')).toContainText('Código inválido. Por favor, tente novamente.');
 
 });
 
 test('Sucessful login', async ({ page }) => {
-  await page.goto('http://paybank-mf-auth:3000/');
-  
+  const loginPage = new LoginPage(page);
+  const dashPage = new DashPage(page);
+
   const user = {
     cpf: '00000014141',
-    senha: '147258'
+    password: '147258'
   }
 
-  await page.getByRole('textbox', { name: 'Digite seu CPF' }).fill(user.cpf);
-  await page.getByRole('button', { name: 'Continuar' }).click();
+  await loginPage.acessaPagina();
+  await loginPage.fillCPF(user.cpf);
+  await loginPage.fillPassword(user.password);
 
-
-  for (const digito of user.senha){
-    await page.getByRole('button', { name: digito }).click();
-  }
-
-  await page.getByRole('button', { name: 'Continuar' }).click();    
-
+  //temp
   await page.waitForTimeout(3000);
   const code = await get2FCode();
 
-  await page.getByRole('textbox', { name: '000000' }).fill(code);
-  await page.getByRole('button', { name: 'Verificar' }).click();
-
-  await expect(page.locator('#account-balance')).toHaveText('R$ 5.000,00');
+  await loginPage.fill2FCode(code);
+  
+  //temp
+  await page.waitForTimeout(3000);
+  expect(await dashPage.getBalance()).toHaveText('R$ 5.000,00');
 });
