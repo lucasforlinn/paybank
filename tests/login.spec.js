5dd2930ec348083917ea9020f02c24a9ddd69512
lucasforlinn <lucasforlinn@gmail.com>
2025-03-04 13:00:24 -0300

    wip: including lib to connect to redis, and change the way to get the 2factor code from redis instead from BD

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -3,6 +3,8 @@ import { get2FCode } from '../support/db.js';
 import { LoginPage } from '../pages/LoginPage.js';
 import { DashPage } from '../pages/DashPage.js';
 
+import { cleanJobs, getJob } from '../support/redis.js';
+
 test('Try to login using a invalid 2F code', async ({ page }) => {
   const loginPage = new LoginPage(page);
   const user = {
@@ -30,12 +32,19 @@ test('Sucessful login', async ({ page }) => {
     password: '147258'
   }
 
+  await cleanJobs();
+
   await loginPage.acessaPagina();
   await loginPage.fillCPF(user.cpf);
   await loginPage.fillPassword(user.password);
 
   await page.getByRole('heading', {name: 'Verificação em duas etapas'}).waitFor({timeout: 3000});
-  const code = await get2FCode(user.cpf);
+  
+  // Buscando o codigo de autenticacao no redis
+  const code = await getJob();
+  
+  // Buscando o codigo de autenticacao no banco
+  // const code = await get2FCode(user.cpf);
 
   await loginPage.fill2FCode(code);
   
